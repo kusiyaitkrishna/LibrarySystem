dd90977989447c945da8e7fe17b2b7eb9ecd71ad
kusiyaitkrishna <krishkusiyait@gmail.com>
2026-03-15 20:18:59 +0545

    Foundation implementation of system with all necessary format

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Custom admin interface for User model.
+    Extends Django's built-in UserAdmin.
+    """
+    
+    # Fields to display in the user list
+    list_display = ['username', 'email', 'user_type', 'is_staff', 'is_active']
+    
+    # Filters in the sidebar
+    list_filter = ['user_type', 'is_staff', 'is_active']
+    
+    # Search functionality
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    
+    # Add user_type to the fieldsets
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('user_type',)}),
+    )
+    
+    # Add user_type when creating a new user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('user_type',)}),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom User model extending Django's AbstractUser.
+    
+    This model adds a user_type field to differentiate between
+    librarians and regular members in the library system.
+    """
+    
+    class UserType(models.TextChoices):
+        LIBRARIAN = 'librarian', 'Librarian'
+        MEMBER = 'member', 'Member'
+    
+    user_type = models.CharField(
+        max_length=20,
+        choices=UserType.choices,
+        default=UserType.MEMBER,
+        help_text='Designates the role of this user in the library system.'
+    )
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+    def __str__(self):
+        return f"{self.username} ({self.get_user_type_display()})"
+    
+    @property
+    def is_librarian(self):
+        """Check if user is a librarian."""
+        return self.user_type == self.UserType.LIBRARIAN
+    
+    @property
+    def is_member(self):
+        """Check if user is a member."""
+        return self.user_type == self.UserType.MEMBER
