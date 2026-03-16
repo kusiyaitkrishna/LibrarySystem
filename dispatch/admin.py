d0b6de0586110b0a79f023a983298f7c7c9c42af
kusiyaitkrishna <krishkusiyait@gmail.com>
2026-03-16 23:33:49 +0545

    Adding Dispatch system

--- dispatch/admin.py
+++ dispatch/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import BorrowRecord
 
-# Register your models here.
+@admin.register(BorrowRecord)
+class BorrowRecordAdmin(admin.ModelAdmin):
+    list_display = ['book', 'member', 'borrow_date', 'due_date', 'return_date', 'status']
+    list_filter = ['status', 'borrow_date', 'due_date']
+    search_fields = ['book__title', 'member__member_id', 'member__user__username']
+    readonly_fields = ['borrow_date']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('book', 'member', 'member__user')
